dc6cee0011d0ba616e46689b19c1cbf8af23405b
fuyang <fu.caillen@gmail.com>
2022-09-21 19:13:32 +0800

    feat(model): 更新分页及条件

--- models/paginate.go
+++ models/paginate.go
@@ -1,20 +1,53 @@
 package models
 
 import (
+	"github.com/cpyun/cpyun-admin-core/tools/search"
 	"gorm.io/gorm"
 )
 
+// MakeCondition 生成查询条件
+func MakeCondition(query interface{}) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		condition := &search.GormCondition{
+			GormPublic: search.GormPublic{},
+			Join:       make([]*search.GormJoin, 0),
+		}
+		search.ResolveSearchQuery("mysql", query, condition)
+		for _, join := range condition.Join {
+			if join == nil {
+				continue
+			}
+			db = db.Joins(join.JoinOn)
+			for k, v := range join.Where {
+				db = db.Where(k, v...)
+			}
+			for k, v := range join.Or {
+				db = db.Or(k, v...)
+			}
+			for _, o := range join.Order {
+				db = db.Order(o)
+			}
+		}
+		for k, v := range condition.Where {
+			db = db.Where(k, v...)
+		}
+		for k, v := range condition.Or {
+			db = db.Or(k, v...)
+		}
+		for _, o := range condition.Order {
+			db = db.Order(o)
+		}
+		return db
+	}
+}
+
+// Paginate 生成分页
 func Paginate(pageSize, pageIndex int) func(db *gorm.DB) *gorm.DB {
-	return func (db *gorm.DB) *gorm.DB {
-		if pageIndex <= 0 {
-			pageIndex = 1
-		}
-
-		if pageSize <= 0 {
-			pageSize = 10
-		}
-
+	return func(db *gorm.DB) *gorm.DB {
 		offset := (pageIndex - 1) * pageSize
+		if offset < 0 {
+			offset = 0
+		}
 		return db.Offset(offset).Limit(pageSize)
 	}
 }
