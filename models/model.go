43e334c928bf5333a21a805ad64871ac7a25d9e9
fuyang <fu.caillen@gmail.com>
2022-09-23 14:07:44 +0800

    refactor: 重新命名基础模型字段名

--- models/model.go
+++ models/model.go
@@ -10,14 +10,14 @@ type Model struct {
 }
 
 type ControlBy struct {
-	CreateBy int `json:"createBy" gorm:"index;comment:创建者"`
-	UpdateBy int `json:"updateBy" gorm:"index;comment:更新者"`
+	CreateBy int `json:"create_by" gorm:"index;comment:创建者"`
+	UpdateBy int `json:"update_by" gorm:"index;comment:更新者"`
 }
 
 type ModelTime struct {
-	CreatedTime time.Time `gorm:"column:created_time; autoUpdateTime; default:CURRENT_TIMESTAMP;<-:create" json:"created_time,omitempty"`
-	UpdatedTime time.Time `gorm:"column:update_time; autoCreateTime; default:CURRENT_TIMESTAMP on update current_timestamp" json:"updated_time,omitempty"`
-	//DeletedTime	gorm.DeletedAt 	`json:"-" gorm:"index;comment:删除时间"`
+	CreateTime time.Time `gorm:"column:create_time; autoUpdateTime; default:CURRENT_TIMESTAMP;<-:create" json:"create_time,omitempty"`
+	UpdateTime time.Time `gorm:"column:update_time; autoCreateTime; default:CURRENT_TIMESTAMP on update current_timestamp" json:"update_time,omitempty"`
+	//DeleteTime	gorm.DeleteAt 	`json:"-" gorm:"index;comment:删除时间"`
 }
 
 // SetCreateBy 设置创建人id
