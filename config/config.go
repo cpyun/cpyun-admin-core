ff48e5effa220810b0acc7269397d97e825aefc2
fuyang <fu.caillen@gmail.com>
2023-07-18 17:16:40 +0800

    style: 配置文件代码优化

--- config/config.go
+++ config/config.go
@@ -42,13 +42,11 @@ func (e *settings) OnChange() {
 
 // Setup
 // @description   Setup 载入配置文件
-// @auth      caillen             时间（2022/7/22   10:00 ）
+// @auth      cpYun             时间（2022/7/22   10:00 ）
 // @param     s         string        "配置文件路径"
 // @param     fs        func          "回调函数"
 // @return
 func Setup(s string, fs ...func()) {
-	//var err error
-
 	Settings = &Config{
 		Application: ApplicationConfig,
 		Database:    DatabaseConfig,
@@ -59,36 +57,20 @@ func Setup(s string, fs ...func()) {
 		Queue:       QueueConfig,
 		Locker:      LockerConfig,
 		JWT:         JwtConfig,
-
-		Extend: ExtendConfig,
-	}
-	_cfg = &settings{
-		Settings:  *Settings,
-		callbacks: fs,
+		Extend:      ExtendConfig,
 	}
 
 	//1. 读取配置
 	driver.NewSource(s)
-
-	//driver.DefaultConfig, err = driver.NewConfig(
-	//	// 绑定实体
-	//	driver.WithEntity(_cfg),
-	//)
-	//if err != nil {
-	//	log.Fatal(fmt.Sprintf("New config object fail: %s", err.Error()))
-	//}
-	//entity := defaultConfig.Options().Entity
-	//driver.WithBind(entity)
-	//fmt.Printf("entity >>>>>>>>>>>>>>> %+v \n", entity)
-
 	driver.WithBind(Settings)
-	//fmt.Printf("Settings >>>>>>>>>>>>>>> %+v \n", Settings)
-	_cfg.Settings = *Settings
 
 	// 绑定单个结构体数据
-	//driver.WithBindKey("application", ApplicationConfig)
 	driver.WithBindKey("extend", Settings.Extend)
 
 	// 初始化配置
+	_cfg = &settings{
+		Settings:  *Settings,
+		callbacks: fs,
+	}
 	_cfg.init()
 }
--- config/driver/viper.go
+++ config/driver/viper.go
@@ -3,50 +3,33 @@ package driver
 import (
 	"errors"
 	"fmt"
-	"log"
-	"os"
-
 	"github.com/fsnotify/fsnotify"
-	"github.com/mitchellh/go-homedir"
-	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 func NewSource(s string) string {
-	//1. 读取配置
-	if s != "" {
-		viper.SetConfigFile(s)
-	} else {
-		home, err := homedir.Dir()
-		cobra.CheckErr(err)
-
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName("settings")
-	}
-
+	viper.SetConfigFile(s)
 	// 获取环境变量
 	viper.AutomaticEnv()
 
 	err := viper.ReadInConfig()
 	notFound := &viper.ConfigFileNotFoundError{}
-
-	switch {
-	case err != nil && !errors.As(err, notFound):
-		cobra.CheckErr(err)
-	case err != nil && errors.As(err, notFound):
-		cobra.CheckErr(notFound.Error())
-		break
-	default:
-		fmt.Fprintln(os.Stdout, "[config] ============> Using config file:", viper.ConfigFileUsed())
+	if errors.As(err, notFound) {
+		fmt.Println("[config] ============> config file not found: ", s)
+		panic(notFound)
+	} else if err != nil {
+		fmt.Println("[config] ============> read in config error: ", err)
+		panic(err)
 	}
 
-	return ""
+	fmt.Println("[config] ============> Using config file:", viper.ConfigFileUsed())
+	return s
 }
 
 func WithBind(e any) {
 	// 绑定数据
 	if err := viper.Unmarshal(e); err != nil {
+		fmt.Println("[config] ============> with bind parameter error: ", err)
 		panic(err)
 	}
 
@@ -55,16 +38,15 @@ func WithBind(e any) {
 	viper.OnConfigChange(func(et fsnotify.Event) {
 		fmt.Println("[config] ============> config file changed:", et.Name)
 		if err := viper.Unmarshal(e); err != nil {
-			//opts.OnChange()
-			fmt.Println(err)
+			fmt.Println("[config] ===========> change config and bind parameter error: ", err)
 		}
 	})
 }
 
-//
+// WithBindKey 绑定单个
 func WithBindKey(key string, rawVal any) {
 	err := viper.UnmarshalKey(key, rawVal)
 	if err != nil {
-		log.Fatal("[config] ============> error :", err.Error())
+		fmt.Println("[config] ============> with bind key error :", err.Error())
 	}
 }
