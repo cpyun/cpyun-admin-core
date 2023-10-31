b6e4a5341fd547b397a9abaeeec3f305b9ce8741
fuyang <fuyang108@gmail.com>
2023-10-31 16:12:31 +0800

    style: optimized code

--- database/open.go
+++ database/open.go
@@ -1,15 +1,17 @@
 package database
 
 import (
-	"gorm.io/driver/clickhouse"
 	"gorm.io/driver/mysql"
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var opens = map[string]func(string) gorm.Dialector{
-	"mysql":    mysql.Open,
-	"postgres": postgres.Open,
+	"mysql": mysql.Open,
+	//"postgres": postgres.Open,
 	//"sqlite3":  sqlite.Open,
-	"clickhouse": clickhouse.Open,
+	//"clickhouse": clickhouse.Open,
+}
+
+func WithOpens(key string, open func(s string) gorm.Dialector) {
+	opens[key] = open
 }
--- storage/queue/redis.go
+++ storage/queue/redis.go
@@ -1,7 +1,7 @@
 package queue
 
 import (
-	"github.com/go-redis/redis/v8"
+	"github.com/redis/go-redis/v9"
 	"github.com/robinjoseph08/redisqueue/v2"
 
 	"github.com/cpyun/cpyun-admin-core/storage"
